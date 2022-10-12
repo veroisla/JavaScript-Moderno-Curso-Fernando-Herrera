f486898a543ddf50124a394cf4b7966db7a4bfd9
Veronica isla <veroislagleaniz@gmail.com>
2022-10-12 12:16:33 +0200

    Remembering console options and window functions: alert, promopt and confirm

---
+++ 01-Fundamentos/assests/js/alerts.js
@@ -0,0 +1,7 @@
+alert('hola mundo');
+
+let nombre = prompt('¿Cual es tu nombre?');
+console.log(nombre);
+
+const seleccion = confirm('Está seguro de borrar esto?');
+console.log(seleccion);
---
+++ 01-Fundamentos/assests/js/console.js
@@ -0,0 +1,8 @@
+let a = 10;
+let b = 5;
+let c = 'Hola ';
+let d = 'Mundo';
+
+console.log({ a, b });
+console.table({ a, b, c, d });
+console.log(c + d);
